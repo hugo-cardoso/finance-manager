b93a0382d47cb8f88defe8346615f9d4268d98de
Hugo Cardoso <hugorafaelmc@gmail.com>
2025-12-16 00:36:07 -0300

    build(frontend): adiciona log de envs no build

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,28 +5,34 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const config = defineConfig({
-  plugins: [
-    devtools(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-  resolve: {
-    alias: {
-      "@tabler/icons-react": path.resolve(__dirname, "node_modules/@tabler/icons-react/dist/esm/icons/index.mjs"),
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  console.log(env);
+
+  return {
+    plugins: [
+      devtools(),
+      nitro(),
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+    ],
+    resolve: {
+      alias: {
+        "@tabler/icons-react": path.resolve(__dirname, "node_modules/@tabler/icons-react/dist/esm/icons/index.mjs"),
+      },
     },
-  },
+  };
 });
 
 export default config;
