3f7450f799694a3466b9d194f9b614e324cf55c4
Hugo Cardoso <hugorafaelmc@gmail.com>
2025-12-17 19:54:36 -0300

    Add refresh token support

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, UseGuards, ValidationPipe } from "@nestjs/common";
 
 import { AuthService } from "./auth.service";
+import { RefreshTokenDto } from "./dto/refresh-token.dto";
 import { SignInDto } from "./dto/sign-in.dto";
 import { SignUpDto } from "./dto/sign-up.dto";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
@@ -18,5 +19,10 @@ export class AuthController {
   @Post("sign-up")
   async signUp(@Body(new ValidationPipe()) signUpDto: SignUpDto) {
     await this.authService.signUp(signUpDto);
+  }
+
+  @Post("refresh")
+  refresh(@Body(new ValidationPipe()) refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshToken(refreshTokenDto.refresh_token);
   }
 }
