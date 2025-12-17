c5085317a043ff9eaa0cd208655ccc0597b694a8
Hugo Cardoso <hugorafaelmc@gmail.com>
2025-12-16 21:54:59 -0300

    chore(frontend): remove os componentes do shadcn e suas dependencias

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,16 +5,12 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const config = defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  console.log(env);
-
+const config = defineConfig(() => {
   return {
     plugins: [
       devtools(),
